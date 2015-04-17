69da36f562dc7c5a1da884111076b970bbeb4d38
Mitja Golouh <mitja.golouh@gmail.com>
2015-04-17 08:36:57 +0200

    Values for dynamic drop down can be generated by a call to bash script

--- src/main/java/com/tekante/jenkins/DynamicParameter.java
+++ src/main/java/com/tekante/jenkins/DynamicParameter.java
@@ -9,6 +9,9 @@ import hudson.model.ParametersDefinitionProperty;
 import hudson.model.StringParameterValue;
 import hudson.util.ListBoxModel;
 import hudson.model.Job;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -21,6 +24,8 @@ import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * String based parameter that supports picking the string from two lists of values
@@ -38,15 +43,22 @@ public class DynamicParameter extends ParameterDefinition {
   public String value = "";
   public String dynamicValue = "";
   public String valueOptions;
+  public String firstBashScript;
   public String dynamicValueOptions;
   public String secondName;
+  public String secondBashScript;
+  public boolean cacheScriptResults;
+  public Map<String, ListBoxModel> dynamicValuesCache;
 
   @DataBoundConstructor
-  public DynamicParameter(String name, String description, String valueOptions, String dynamicValueOptions, String secondName) {
+  public DynamicParameter(String name, String description, String valueOptions, String firstBashScript, String dynamicValueOptions, String secondName, String secondBashScript, boolean cacheScriptResults) {
     super(name, description);
     this.secondName = secondName;
     this.valueOptions = valueOptions;
     this.dynamicValueOptions = dynamicValueOptions;
+    this.firstBashScript = firstBashScript;
+    this.secondBashScript = secondBashScript;
+    this.cacheScriptResults = cacheScriptResults;
   }
 
   @Extension
@@ -86,8 +98,21 @@ public class DynamicParameter extends ParameterDefinition {
 
       DynamicParameter dp = this.getDynamicParameter(name);
       if (dp != null) {
+        if (dp.firstBashScript == null ? "" != null : !dp.firstBashScript.equals(""))
+        {
+            String csv = execOsScriptAndReturnFirstLineOfOutput(dp.firstBashScript);
+            for (String s : csv.split(",")) {
+              m.add(s);
+            }
+        }
+        else
+        {
         for (String s : dp.valueOptions.split("\\r?\\n")) {
           m.add(s);
+        }
+      }
+        if (dp.cacheScriptResults){
+            dp.dynamicValuesCache = new HashMap<String, ListBoxModel>();
         }
       }
       return m;
@@ -98,12 +123,31 @@ public class DynamicParameter extends ParameterDefinition {
 
       DynamicParameter dp = this.getDynamicParameter(name);
       if (dp != null) {
+            if (dp.secondBashScript == null ? "" != null : !dp.secondBashScript.equals(""))
+            {
+                // check the cache if we already have values for this key
+                if (dp.cacheScriptResults && dp.dynamicValuesCache.containsKey(value)){
+                    m = dp.dynamicValuesCache.get(value);
+                    return m;
+                }
+
+                String csv = execOsScriptAndReturnFirstLineOfOutput(dp.secondBashScript + " " + value);
+                for (String s : csv.split(",")) {
+                    m.add(s);
+                }
+
+                // add new value to cache
+                if (dp.cacheScriptResults) dp.dynamicValuesCache.put(value, m);
+            }
+            else
+            {
         for (String s : dp.dynamicValueOptions.split("\\r?\\n")) {
           if (s.indexOf(value) == 0) {
             String[] str = s.split(":");
             m.add(str[1]);
           }
         }
+      }
       }
       return m;
     }
@@ -122,5 +166,27 @@ public class DynamicParameter extends ParameterDefinition {
     LOG.warning(getName() + ": " + value[0] + "\n");
     LOG.warning(this.secondName + ": " + dynamicValue[0] + "\n");
     return new DynamicParameterValue(getName(), value[0], this.secondName, dynamicValue[0]);
+  }
+
+  private static String execOsScriptAndReturnFirstLineOfOutput(String scriptName) {
+    try {
+        Process proc = Runtime.getRuntime().exec(scriptName);
+        BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        proc.waitFor();
+
+        String s;
+        while ((s = in.readLine()) != null) {
+            return s;
+            // Logger.getLogger(ExtendedChoiceParameterDefinition.class.getName()).log(Level.INFO, null, in.readLine());
+        }
+        } catch (IOException ex) {
+            LOG.severe(ex.getMessage());
+            return ("error");
+        }
+        catch (InterruptedException ex) {
+           LOG.severe(ex.getMessage());
+           return ("error");
+        }
+        return null;
   }
 }
